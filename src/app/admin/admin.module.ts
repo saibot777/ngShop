fb2724282d7cd7a7e0a8cc5b79fac6288d6dfd1e
saibot777 <stefantrajkovic777@gmail.com>
2017-05-09 02:08:27 +0200

    routes setup, admin area created

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
 import {LoginComponent} from "./login/login.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {AdminMenuComponent} from "./adminMenu/admin-menu.component";
+import {AdminComponent} from "./adminComponent/admin.component";
+import {UserService} from "./adminShared/user.service";
+
+const AdminRoutes: Routes = [
+    {
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'signup', component: SignUpComponent },
+            { path: '', component: AdminMenuComponent, canActivate: [UserService] }
+        ]
+    }
+];
 
 
 @NgModule({
     imports: [
         BrowserModule,
+        RouterModule.forChild(AdminRoutes),
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    exports: [
+      RouterModule
     ],
     declarations: [
         LoginComponent,
-        SignUpComponent
+        SignUpComponent,
+        AdminMenuComponent,
+        AdminComponent
+    ],
+    providers: [
+        UserService
     ]
 })
 export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { NavComponent } from './shared/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { AppRoutingModule } from './shared/app.routing';
+import {AdminModule} from "./admin/admin.module";
 
 @NgModule({
     imports: [
         BrowserModule,
+        AdminModule,
         AppRoutingModule
     ],
     declarations: [
